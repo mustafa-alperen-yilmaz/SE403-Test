ad74560465e58f6aa2c2ed5db4fa7065bb50803e
kutay-o <kutayondersev@gmail.com>
2021-03-10 21:58:45 +0300

    test

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main() {
     printf("Hello, World!\n");
     printf("Development branch created BoldPilot ");
     printf("Development branch created Onur ");
+    printf("Development branch created Kutay ");
     return 0;
 }
